dd5c90c31d6e41ecf5dc97a906c16dcd54496387
nelly <nellycita@live.de>
2021-06-22 20:01:36 +0200

    lab solved

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0 
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a, b){return a - b});
+    this.length ++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length) {
+      return this.items [pos]
+    }
+    else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0){
+      return this.items[this.length -1 ]
+    }
+    else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0){
+      return this.items[0]
+    }
+    else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0
+    for (let i = 0; i < this.items.length; i++){
+      sum = sum + this.items [i]
+    }
+    return sum 
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length > 0){
+      let average = this.sum() / this.items.length
+      return average
+    }
+    else {
+      throw new Error('EmptySortedList');
+    }
+    
+  }
+
 }
 
 module.exports = SortedList;
